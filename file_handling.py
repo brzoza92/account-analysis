21c18a277cc9dc754f54595606fd03ef43563509
brzoza92 <71319639+brzoza92@users.noreply.github.com>
2020-09-15 13:29:46 +0200

    Created file_handling.py and test file
    file_handling.py to handle a class with CSV file operations. Create class generated

--- file_handling.py
+++ file_handling.py
@@ -1,3 +1,17 @@
 import pandas as pd
 
-#some random stuff
+class File:
+    def __init__(self):
+        pass
+
+    def Create(self, name):
+        columns = {
+            "Data operacji": 0, "Data waluty": 1,
+            "Typ transakcji": 2, "Kwota": 3,
+            "Waluta": 4, "Saldo po transakcji": 5,
+            "Opis transakcji": 6, "Nazwa": 7, 
+            "Tytuł": 8, "Info1": 9, "Info2": 10
+        }
+        df1 = pd.DataFrame(columns, index = [0])
+        df1.to_csv(name, index = False, encoding="latin2")
+
---
+++ test_file.py
@@ -0,0 +1,7 @@
+from file_handling import File
+
+
+new_account = File()
+
+
+new_account.Create("tabela1.csv")
