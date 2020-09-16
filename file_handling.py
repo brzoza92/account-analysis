b36c98836c4a030d7d5ccd68604dedc12ff825c0
brzoza92 <71319639+brzoza92@users.noreply.github.com>
2020-09-16 13:47:51 +0200

    Add new data file and split costs on months
    Improved addition of data. Finding difference beetwen files and ereasing columns that are not expected in main file.
    
    SplitMonth method gives out dictionary with costs taken in each month according to file

--- file_handling.py
+++ file_handling.py
@@ -8,7 +8,15 @@ class File:
         """
         self.path = name
         self.encoding = "latin2"
-        
+        self.columns = {
+            "Data operacji": 0, "Data waluty": 1,
+            "Typ transakcji": 2, "Kwota": 3,
+            "Waluta": 4, "Saldo po transakcji": 5,
+            "Opis transakcji": 6, "Nazwa": 7, 
+            "Tytuł": 8, "Info1": 9, "Info2": 10
+        }
+
+
         try:
             self.ReadMain()
 
@@ -17,6 +25,40 @@ class File:
             print("File is not existing yet.")
             self.Create()
             
+    def __getMonthDict__(self):
+        """
+        Reinitialize dictionary of months. 
+        """
+        self.months = {}
+        for i in range(1,13):
+            if i < 10:
+                month_no = "0"+str(i)
+            else:
+                month_no = str(i)
+            month_no = "-"+ month_no + "-"
+            self.months[month_no] = 0
+        return self.months
+    def __getFileLength__(self):
+        """
+        Returns the length of main file.
+        """    
+        self.length = self.main_file[self.main_file.columns[0]].count()
+        return self.length
+    def __columnsCmp__(self, file):
+        new_columns = file.columns
+        main_columns = list(self.columns)
+
+        return len(main_columns) != len(new_columns)    
+    
+    def __columnsDiff__(self, file):
+        new_columns_diff = len(file.columns)
+        main_columns_diff = len(list(self.columns))
+        to_erease = list(range(main_columns_diff,new_columns_diff))
+        to_erease_name = []
+        for column in to_erease:
+            to_erease_name.append(file.columns[column])
+        return to_erease_name   
+
 
     def ReadMain(self):
         """
@@ -30,14 +72,6 @@ class File:
         """
         Creates new data file using instance name. Name needs to have .csv suffix.
         """
-
-        self.columns = {
-            "Data operacji": 0, "Data waluty": 1,
-            "Typ transakcji": 2, "Kwota": 3,
-            "Waluta": 4, "Saldo po transakcji": 5,
-            "Opis transakcji": 6, "Nazwa": 7, 
-            "Tytuł": 8, "Info1": 9, "Info2": 10
-        }
         df1 = pd.DataFrame(self.columns, index = [0])
         df1.drop(axis = 0, index = df1.index[0], inplace = True)
         df1.to_csv(self.path, index = False, encoding=self.encoding)
@@ -50,10 +84,43 @@ class File:
         """
         
         df_new_data = pd.read_csv(newfile, encoding = self.encoding)
+
+        if self.__columnsCmp__(df_new_data):         #check if same type of csv data
+            df_new_data.drop(columns = self.__columnsDiff__(df_new_data), inplace = True)
+        
         df_new_data.to_csv(self.path, index = False, header = False, mode = 'a', encoding = self.encoding)
         
         self.ReadMain()     #Main file needs to be read again (refreshed) after writing to file done above.
         self.main_file.drop_duplicates(inplace = True)
         self.main_file.to_csv(self.path, index = False, encoding = self.encoding)
 
+
         return None
+    def SplitMonths(self):
+        """
+        Presents costs taken in split for months.
+
+        During development add a year selection. In default, actual year.
+        """
+        self.__getMonthDict__()
+        self.ReadMain()    
+
+        #invest = ["revolut", "brzozowski"]
+
+        dateColumn = self.main_file[self.main_file.columns[0]]
+        amountColumn = self.main_file[self.main_file.columns[3]]
+        #transName = self.main_file[self.main_file.columns[7]]
+        for m in self.months:
+            sum = 0
+            rows = []
+            for value in range(0, self.__getFileLength__()):
+                if (m in dateColumn[value]) and (amountColumn[value] < 0):
+                    # if any(cond in transName[value].lower() for cond in invest):
+                    #     pass
+                    # else:
+                    rows.append(value)    #save what row fits to condition above
+            for ids in rows:
+                sum += amountColumn[ids]
+            self.months[m] =  round(sum,2) * -1      
+        
+        return self.months
--- test_file.py
+++ test_file.py
@@ -5,7 +5,10 @@ new_account = File("tabela1.csv")
 
 #new_account.Create()
 
-new_account.NewData("history_csv_20200914_181117.csv")
+new_account.NewData("history_csv_20200914_182105.csv")
+#new_account.NewData("history_csv_20200914_181117.csv")
+
+#print(new_account.SplitMonths())
 
 
 #print(new_account.ReadMain().head())
