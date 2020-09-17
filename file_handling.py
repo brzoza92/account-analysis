ab922b85651c547878e24824ef9b74495c051d7f
brzoza92 <71319639+brzoza92@users.noreply.github.com>
2020-09-17 13:54:21 +0200

    Json file handlign
    Added methods to add new categories, new place names and also to add transaction descriptions

--- file_handling.py
+++ file_handling.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 class File:
     def __init__(self, name):
@@ -13,7 +14,8 @@ class File:
             "Typ transakcji": 2, "Kwota": 3,
             "Waluta": 4, "Saldo po transakcji": 5,
             "Opis transakcji": 6, "Nazwa": 7, 
-            "Tytuł": 8, "Info1": 9, "Info2": 10
+            "Tytuł": 8, "Info1": 9, "Info2": 10, 
+            "Kategoria": 11, "Placówka": 12
         }
 
 
@@ -45,20 +47,25 @@ class File:
         self.length = self.main_file[self.main_file.columns[0]].count()
         return self.length
     def __columnsCmp__(self, file):
+        """
+        Compare amount of columns of files.
+        """
         new_columns = file.columns
         main_columns = list(self.columns)
 
         return len(main_columns) != len(new_columns)    
     
     def __columnsDiff__(self, file):
+        """
+        Returning the array with names of columns to be deleted out of new file.
+        """
         new_columns_diff = len(file.columns)
         main_columns_diff = len(list(self.columns))
         to_erease = list(range(main_columns_diff,new_columns_diff))
         to_erease_name = []
         for column in to_erease:
             to_erease_name.append(file.columns[column])
-        return to_erease_name   
-
+        return to_erease_name  
 
     def ReadMain(self):
         """
@@ -124,3 +131,137 @@ class File:
             self.months[m] =  round(sum,2) * -1      
         
         return self.months
+
+class dbFile:
+    def __init__(self):
+        """
+        Initialization of class handling the json file with data to sort acount values for type and place name
+        """
+        self.file_name = "dbFile.txt"
+        self.categories = "categories"
+
+        try:
+            self.__Open__()
+
+        except FileNotFoundError:
+            pass
+            print("File is not existing yet.")
+            self.__Create__()
+
+    def __Open__(self):
+        """
+        Opens the dbFile
+        """
+        with open(self.file_name, 'r') as infile:
+            self.file = json.load(infile)
+
+        return self.file    
+
+    def __Write__(self, data):
+        """
+        Writes the dbFile
+        """
+        with open(self.file_name, 'w') as outfile:
+            json.dump(data, outfile, indent = 4)
+
+        return None    
+
+    def __Create__(self):
+        """
+        Creating of default file in case if the file is lost.
+        """
+        default_data = {}
+        default_data[self.categories] = [
+            "Spozywcze", "Rachunki", "Paliwo", "Restauracje",
+            "Odziez", "Platnosc przez internet", "Dla psa"
+            ]
+        for category in default_data[self.categories]:
+            default_data[category] = []
+            default_data[category].append({"Nazwa placowki": "", "Opis transakcji": []})
+
+        self.__Write__(default_data)
+
+        return None
+
+    def __getCategoriesList__(self):
+        file = self.__Open__()
+        return file[self.categories]
+
+    def __getDefDictKeys__(self, data):
+        return list(data[0].keys())
+
+    def selectCategory(self):
+        """
+        Returns string with selected category, to search through dictionary
+        """
+        print("Lista kategorii:")
+        categories = self.__getCategoriesList__()
+        for i,j in enumerate(categories):
+            print(str(i) + "- " + j)
+        category = int(input("Wybierz kategorie: "))
+        return categories[category]
+
+    def selectPlace(self, category):
+        """
+        Returns index of dictionary with selected place
+        """
+
+        file_place = self.__Open__()
+
+        names_key = self.__getDefDictKeys__(file_place[category])[0]
+        i = 0
+        for dict in file_place[category]:
+            if i > 0:
+                print(str(i) +"- " +dict.get(names_key))
+            i += 1    
+        if (i >= 2):
+            place = int(input("Wybierz miejsce: "))       
+        else:
+            print("Brak placówek. Musisz dodać miejsca do kategorii " + category)
+            place = 0
+        return place
+
+    def AddCategory(self):
+        """
+        Adding of category to json file
+        """
+
+        new_category = input("Podaj nazwe kategorii: ")
+        update_category = self.__Open__()
+        update_category[self.categories].append(new_category)
+
+        self.__Write__(update_category)
+
+        return None
+
+    def AddPlace(self):
+        """
+        Adding new place in specified category
+        """
+        update_attributes = self.__Open__()
+        category = self.selectCategory()
+        
+        
+        new_name = input("Podaj nazwe miejsca: ")
+
+        section = update_attributes[category]
+        new_dict = {self.__getDefDictKeys__(section)[0]: new_name, self.__getDefDictKeys__(section)[1]: []}
+        section.append(new_dict)
+
+        self.__Write__(update_attributes)
+        return None
+
+    def AddDescription(self):
+        """
+        Adding new description for specified place
+        """
+        update_description = self.__Open__()
+        category = self.selectCategory()
+        key = self.__getDefDictKeys__(update_description[category])[1]
+        place = self.selectPlace(category)
+        if place > 0:
+            new_description = input("Podaj opis: ")
+            update_description[category][place].get(key).append(new_description)
+
+            self.__Write__(update_description)
+        return None
--- test_file.py
+++ test_file.py
@@ -1,14 +1,20 @@
-from file_handling import File
+from file_handling import File, dbFile
 
 
-new_account = File("tabela1.csv")
+new_account = File("tabela2.csv")
 
 #new_account.Create()
 
-new_account.NewData("history_csv_20200914_182105.csv")
+#new_account.NewData("history_csv_20200914_182105.csv")
 #new_account.NewData("history_csv_20200914_181117.csv")
 
-#print(new_account.SplitMonths())
+#rint(new_account.SplitMonths())
 
 
 #print(new_account.ReadMain().head())
+
+new_json = dbFile()
+
+new_json.AddDescription()
+
+
