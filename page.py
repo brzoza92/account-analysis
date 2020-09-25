0f84a57fdb4ae4e424d68b59a42adc5deb58fb90
brzoza92 <71319639+brzoza92@users.noreply.github.com>
2020-09-25 12:50:14 +0200

    started to work on html GUI

--- page.py
+++ page.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "a67c6cc35e60b993eed3ec744bbc84da"
 
 @app.route("/")
 def home():
@@ -11,6 +13,11 @@ def home():
 def about():
     return render_template("about.html", title = "About")
 
+
+@app.route("/selection", methods = ['POST'])
+def selection():
+    text_in = request.form.get('text_in')
+    return render_template("selection.html", title = "Selection", text_input = text_in)
 
 if __name__ == "__main__":
     app.run(debug = True)
